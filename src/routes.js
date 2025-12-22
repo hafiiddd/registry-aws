4c056fcd54661939b69926d44fe77ff3326b5f97
hafid <hafid.akhyar0411@gmail.com>
2025-12-22 23:28:29 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const routes = (handler) => [
+ {
+     method: 'POST',
+     path: '/predict',
+     handler: handler.getPredictResult,
+     options: {
+       payload: {
+         allow: 'multipart/form-data',
+         multipart: true,
+       }
+     }
+   },
+];
+
+module.exports = routes;
